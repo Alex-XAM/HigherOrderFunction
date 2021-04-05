d3b4d632c594c6e90368a4417cf136404b674c0f
root <root@HOME-PC.localdomain>
2021-04-05 14:25:24 +0300

    Labs 7

--- Exercises/3-wrapper.js
+++ Exercises/3-wrapper.js
@@ -1,5 +1,20 @@
 'use strict';
 
-const contract = (fn, ...types) => null;
+const contract = (fn, ...types) => (...args) => {
+  for (let i = 0; i < args.length; i++) {
+    const argType = typeof args[i];
+    const expectedType = types[i].name.toLowerCase();
+    if (argType !== expectedType) {
+      throw new TypeError(`expected type ${expectedType}`);
+    }
+  }
+  const res = fn(...args);
+  const resType = typeof res;
+  const expectedType = types[types.length - 1].name.toLowerCase();
+  if (resType !== expectedType) {
+    throw new TypeError(`expected type ${expectedType}`);
+  }
+  return res;
+};
 
 module.exports = { contract };
